98bc5c42dc7e7f153bbe020ccd916e0d74ed7ae8
Julio Cardona <jucardona71599@gmail.com>
2022-01-24 00:16:16 -0800

    added read button

--- file.js
+++ file.js
@@ -1,9 +1,5 @@
 //create array of books
 const myLibrary = []
-// const {v4:uuidv4} = require('uuid');
-addBookToLibrary("Gloves Off", "Lowell Cohn", "240", true)
-addBookToLibrary("Charlotte's Web", "E.B. Webber", "192", false)
-
 
 //create constructor for books
 function Book(title, author, pages, read) {
@@ -31,9 +27,14 @@ function displayAll() {
         bookCard.setAttribute("class", "bookCard")
         display.appendChild(bookCard);
         const deleteBook = addDelete(book.id)
-        bookCard.textContent += `Title: ${book.title} \r\n Author: ${book.author} \r\n Pages: ${book.pages}`;
+        const readBook = addReadBox(book.id)
+        let status = (book.read==true) ? "Yes" :
+        (book.read==false) ? "No" : ""
+        bookCard.textContent += `Title: ${book.title} \r\n Author: ${book.author} \r\n Pages: ${book.pages} \r\n Read:${status}`;
+        if (book.read==true) {readBook.textContent = "not read"}
+        else if (book.read==false) {readBook.textContent = "read"}
         deleteBook.textContent = "delete"
-        // deleteBook.value = book.id
+        bookCard.appendChild(readBook)
         bookCard.appendChild(deleteBook)
         i++
     }
@@ -52,15 +53,21 @@ function deleteBook(id) {
     console.table(myLibrary)
     displayAll()
 }
-// function findBook(title) {
-//     if (myLibrary.length === 0 || myLibrary.length === null) { return }
-//     for (let book of myLibrary)
-//         if (title === this.title) {
-//             console.log(myLibrary.indexOf(book))
-//             return this.number
-//         }
-// }
-
+function addReadBox(id) {
+    const readButton = document.createElement("button")
+    readButton.setAttribute("class", "chkbx")
+    readButton.addEventListener("click", () => {
+        changeRead(id)
+    })
+    return readButton
+}
+function changeRead(id) {
+    const capture = myLibrary.find(Book => Book.id === id)
+    console.log(capture.read)
+    if (capture.read == true) {capture.read=false}
+    else if (capture.read == false) {capture.read=true}
+    displayAll()
+}
 //allow user to add a new book
 const openform = document.querySelector("button.newButton")
 const closeform = document.querySelector("button.closeButton")
@@ -92,4 +99,3 @@ submit.addEventListener("click", (e) => {
     e.preventDefault();
     closeForm()
 })
-displayAll()
