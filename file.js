7816c2c718865a59d55a1f350595f3be190bf001
Julio Cardona <jucardona71599@gmail.com>
2022-01-23 21:37:54 -0800

    added delete button and some styling

--- file.js
+++ file.js
@@ -1,7 +1,9 @@
 //create array of books
-let myLibrary = []
+const myLibrary = []
+// const {v4:uuidv4} = require('uuid');
 addBookToLibrary("Gloves Off", "Lowell Cohn", "240", true)
 addBookToLibrary("Charlotte's Web", "E.B. Webber", "192", false)
+
 
 //create constructor for books
 function Book(title, author, pages, read) {
@@ -9,19 +11,56 @@ function Book(title, author, pages, read) {
     this.author = author
     this.pages = pages
     this.read = read
+    this.id = myLibrary.length
 }
 //store new book objects into array
 function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
     console.log(book);
     myLibrary.push(book);
+    displayAll()
 }
 // loop through array to display each book
+
 function displayAll() {
-        for (let book of myLibrary) {
-        console.log(`${book.title} is by ${book.author} and contains ${book.pages} pages.`)
+    let display = document.querySelector("div.display")
+    display.textContent = ""
+    let i = 0
+    for (let book of myLibrary) {
+        const bookCard = document.createElement("div");
+        bookCard.setAttribute("class", "bookCard")
+        display.appendChild(bookCard);
+        const deleteBook = addDelete(book.id)
+        bookCard.textContent += `Title: ${book.title} \r\n Author: ${book.author} \r\n Pages: ${book.pages}`;
+        deleteBook.textContent = "delete"
+        // deleteBook.value = book.id
+        bookCard.appendChild(deleteBook)
+        i++
     }
 }
+function addDelete(id) {
+    const deleteButton = document.createElement("button")
+    deleteButton.setAttribute("class", "delete")
+    deleteButton.addEventListener("click", () => {
+        deleteBook(id)
+    })
+    return deleteButton
+}
+function deleteBook(id) {
+    const capture = myLibrary.findIndex(Book => Book.id === id) 
+    myLibrary.splice(capture, 1);
+    console.table(myLibrary)
+    displayAll()
+}
+// function findBook(title) {
+//     if (myLibrary.length === 0 || myLibrary.length === null) { return }
+//     for (let book of myLibrary)
+//         if (title === this.title) {
+//             console.log(myLibrary.indexOf(book))
+//             return this.number
+//         }
+// }
+
 //allow user to add a new book
 const openform = document.querySelector("button.newButton")
 const closeform = document.querySelector("button.closeButton")
@@ -35,21 +74,22 @@ openform.addEventListener("click", function () {
 })
 closeform.addEventListener("click", () => {
     closeForm()
-    })
+})
 
 function closeForm() {
     form.classList.remove("active")
-    overlay.classList.remove("active") 
+    overlay.classList.remove("active")
 }
-submit.addEventListener("click", () => {
+submit.addEventListener("click", (e) => {
     let newTitle = document.getElementById("title").value
     let newAuthor = document.getElementById("author").value
     let newPages = document.getElementById("pages").value
     let newRead
-    if (document.getElementById("read").checked == true) {newRead = true}
-    else {newRead=false}
+    if (document.getElementById("read").checked == true) { newRead = true }
+    else { newRead = false }
+    console.log(newTitle, newAuthor, newPages, newRead)
     addBookToLibrary(newTitle, newAuthor, newPages, newRead)
-    displayAll()
+    e.preventDefault();
+    closeForm()
 })
-
-    
+displayAll()
